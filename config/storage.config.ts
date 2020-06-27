b06da0b7730e7cc83ffe3f572860965337879a66
djkrstovic <krstovicdjo@gmail.com>
2020-06-27 02:34:05 +0200

    Implement: statically served phtoos

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -3,6 +3,8 @@ export const StorageConfig = {
     photoMovie:{ // MOVIE
 
         destination: '../storage/photos/movie/',
+        urlPrefix: '/assets/moviePhotos',
+        maxAge:1000*60*60*24*7,// 7 dana
         maxSize: 1024*1024*3, // u bajtovima = 3MB // za proveru error too large file promeniti na 10 max file size
         resize: {
             thumb: {
@@ -21,6 +23,8 @@ export const StorageConfig = {
     photoEpisode:{ // EPISODE
 
         destination: '../storage/photos/episode/',
+        urlPrefix: '/assets/episodePhotos',
+        maxAge:1000*60*60*24*7,// 7 dana
         maxSize: 1024*1024*3, // u bajtovima = 3MB // za proveru error too large file promeniti na 10 max file size
         resize: {
             thumb: {
@@ -39,6 +43,8 @@ export const StorageConfig = {
     photoTvSeries:{ // TV SERIES
 
         destination: '../storage/photos/tv-series/',
+        urlPrefix: '/assets/tvSeriesPhotos',
+        maxAge:1000*60*60*24*7,// 7 dana
         maxSize: 1024*1024*3, // u bajtovima = 3MB // za proveru error too large file promeniti na 10 max file size
         resize: {
             thumb: {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { StorageConfig } from 'config/storage.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(StorageConfig.photoMovie.destination, {
+    prefix: StorageConfig.photoMovie.urlPrefix,
+    maxAge: StorageConfig.photoMovie.maxAge,
+    index: false,
+  })
+
+  app.useStaticAssets(StorageConfig.photoEpisode.destination, {
+    prefix: StorageConfig.photoEpisode.urlPrefix,
+    maxAge: StorageConfig.photoEpisode.maxAge,
+    index: false,
+  })
+
+  app.useStaticAssets(StorageConfig.photoTvSeries.destination, {
+    prefix: StorageConfig.photoTvSeries.urlPrefix,
+    maxAge: StorageConfig.photoTvSeries.maxAge,
+    index: false,
+  })
+
   await app.listen(3000);
 }
 bootstrap();
